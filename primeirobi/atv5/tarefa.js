bb77265edc161c93dbf4dd91951d99b27b905432
CamilaPiccinato <Cpvian@minha.fag.edu.bm>
2026-03-30 19:19:14 -0300

    tarefa 5

---
+++ primeirobi/atv5/tarefa.js
@@ -0,0 +1,9 @@
+const valorConta =  100;
+const percentualGorjeta = 5; 
+
+const valorGorjeta = valorConta * (percentualGorjeta / 100);
+
+const valorTotal = valorGorjeta + valorConta;
+
+console.log("Valor da conta R$:" + valorConta, "Gorjeta (5%) R$:" + valorGorjeta, "Total a pagar R$:" + valorTotal);
+
